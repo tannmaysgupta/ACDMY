11e2ea4c329d440b18e8cab44c2d1f62d0dc78c9
Tannmay S Gupta <tanmaygup123@gmail.com>
2020-02-24 06:21:13 +0530

    new scenes and locomotion

---
+++ Assets/ACDMY/Scripts/LookDownMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookDownMenu : MonoBehaviour
+{
+
+    public Transform myCamera;
+
+    public float toggleAngle = 40.0f;
+
+    public bool showMenu;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myCamera.eulerAngles.x >= toggleAngle && myCamera.eulerAngles.x < 90.0f)
+        {
+            showMenu = true;
+        }
+        else
+        {
+            showMenu = false;
+        }
+
+        if (showMenu)
+        {
+            Debug.Log("Showing Menu");
+        }
+    }
+}
---
+++ Assets/ACDMY/Scripts/PlayerWalk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalk : MonoBehaviour
+{
+    public Transform[] movementPoints;
+    
+    public int playerSpeed;
+    public Vector3 pointPos;
+
+    public GameObject movPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementPoints = new Transform[transform.childCount];
+        int i = 0;
+
+        foreach (Transform t in transform)
+        {
+            movementPoints[i++] = t;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position = transform.position + pointPos * playerSpeed * Time.deltaTime;
+        }
+    }
+
+    //public void findPos(movPoint)
+    //{
+      //  pointPos = gameObject.transform.position;
+        
+    //}
+}
